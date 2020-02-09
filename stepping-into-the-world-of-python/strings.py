fc48ea5076a27224677573991bfc2ea24292d662
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-09 11:14:15 +0100

    catalog structure corrected

---
+++ stepping-into-the-world-of-python/strings.py
@@ -0,0 +1,11 @@
+print('Today is a good day to learn Python')
+print('Python is fun')
+print("Python's string is easy to use")
+print('We can even include "quotes" in strings')
+print('Hello' + ' World!')
+
+greeting = "Hello"
+name = input("Enter your name: ")
+
+# Greeting will be printed
+print(greeting + ' ' + name + '!')
