5c9b4d87fbe2c64243bdaf85b7846194277bd53b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-10 08:30:53 +0100

    first shot of operators

---
+++ stepping-into-the-world-of-python/operators.py
@@ -0,0 +1,14 @@
+a = 12
+b = 3
+
+print(a + b)    # 15
+print(a - b)    # 9
+print(a * b)    # 36
+print(a / b)    # 4.0
+print(a // b)   # 4 integer division, rounded down towards minus infinity
+print(a % b)    # 0 modulo: the remainder after integer division
+
+print()
+
+for i in range(1, a // b):
+    print(i)
