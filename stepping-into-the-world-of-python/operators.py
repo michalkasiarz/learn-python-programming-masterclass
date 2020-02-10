a304eaf4a23fa3f9dafbeee857b1ec4933c54746
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-10 09:15:27 +0100

    operators precedence

--- stepping-into-the-world-of-python/operators.py
+++ stepping-into-the-world-of-python/operators.py
@@ -12,3 +12,18 @@ print()
 
 for i in range(1, a // b):
     print(i)
+
+# operator precedence
+
+
+print(a + b / 3 - 4 * 12)
+print((((a + b) / 3) - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print()
+
+print(a / (b * a) / b)
