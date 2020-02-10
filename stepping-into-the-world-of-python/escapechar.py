45c3289756c1228966b57d3f7b056ba0b87151a9
michalkasiarz <42114632+michalkasiarz@users.noreply.github.com>
2020-02-10 07:08:08 +0100

    escape characters

---
+++ stepping-into-the-world-of-python/escapechar.py
@@ -0,0 +1,19 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet owner said "No, no, \'e\'s uh,... he\'s resting.')
+print("""The pet owner said "No, no, 'e's uh,... he's resting.""")
+
+anotherSplitString = """This string has been
+split over
+several
+lines"""
+
+print(anotherSplitString)
+
+print('Number 1\tThe Larch\nNumber 2\tThe Horse Chestnut')
+print('C:\\Users\\Tim\\Notes.txt')
+print(r'C:\Users\Tim\Notes.txt')
