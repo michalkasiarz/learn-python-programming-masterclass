e8851d4a55d85b9d889e9b1ee5e3356fc553a0f2
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-10 11:02:21 +0100

    negative indexing

--- stepping-into-the-world-of-python/strings2.py
+++ stepping-into-the-world-of-python/strings2.py
@@ -8,3 +8,33 @@ print()
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
+
+# negative indexing
+
+print()
+print(parrot[-11])
+print(parrot[-10])
+print()
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+# negative indexing with subtraction
+
+print()
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print()
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
+
+# negative indexing with len() function
+
+print()
+print(parrot[3 - len(parrot)])
+print(parrot[4 - len(parrot)])
+print()
+print(parrot[3 - len(parrot)])
+print(parrot[6 - len(parrot)])
+print(parrot[8 - len(parrot)])
