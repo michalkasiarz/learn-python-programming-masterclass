c16aee2cd98795c0b012746b78cb506c838d14ff
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-12 11:05:44 +0100

    one more checking for guessing game

--- program-flow-control-in-python/GuessingGame.py
+++ program-flow-control-in-python/GuessingGame.py
@@ -5,7 +5,17 @@ guess = int(input())
 
 if guess < answer:
     print('Too low!')
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it!")
+    else:
+        print('Sorry, you\'ve not guessed correctly')
 elif guess > answer:
     print('Too high!')
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it!")
+    else:
+        print('Sorry, you\'ve not guessed correctly')
 else:
     print('You got it first time!')
