c126d7556184d44ea6a56d5e1d5275ddbcc466e1
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-12 08:16:52 +0100

    checking conditions with elif

--- program-flow-control-in-python/Blocks.py
+++ program-flow-control-in-python/Blocks.py
@@ -12,6 +12,8 @@ else:
 # checking the condition the other way
 if age < 18:
     print("You are not old enough to vote. Please come back in {0} years.".format(18 - age))
+elif age == 900:
+    print("Sorry, Yoda, you die in return in Return of the Jedi.")
 else:
     print("You are old enough to vote.")
     print("Please put an X in the box.")
