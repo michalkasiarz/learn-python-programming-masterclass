f16a49865172fb8747d3b8963c6d597ca2c5ac20
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-12 15:03:28 +0100

    True and False boolean values

---
+++ program-flow-control-in-python/TrueFalse.py
@@ -0,0 +1,10 @@
+# True and False
+
+day = "Friday"
+temperature = 30
+raining = False
+
+if (day == "Saturday" and temperature > 27) or not raining:
+    print("Go swimming!")
+else:
+    print("Learn Python!")
