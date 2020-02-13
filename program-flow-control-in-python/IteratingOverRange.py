105c673900ea924efc2a5c27fe1f58b4ae355e78
michalkasiarz <michal.kasiarz@post.com>
2020-02-13 15:29:26 +0100

    interating over a range backwards

--- program-flow-control-in-python/IteratingOverRange.py
+++ program-flow-control-in-python/IteratingOverRange.py
@@ -2,3 +2,14 @@
 
 for num in range(1, 21):    # range is also UP TO BUT NOT INCLUDING the end value
     print(f'i is now {num}')
+
+print()
+
+
+for num in range(0, 21, 2):  # iterating with step 2
+    print(f'i is now {num}')
+
+print()
+
+for num in range(10, 0, -2):  # iterating with step 2 backwards
+    print(f'i is now {num}')
