efd46b696b7de77190962431fc9f0b8caa4271b9
michalkasiarz <michal.kasiarz@post.com>
2020-02-14 10:23:06 +0100

    if block added to while loop

--- program-flow-control-in-python/Adventure.py
+++ program-flow-control-in-python/Adventure.py
@@ -3,5 +3,8 @@ available_exits = ['north', 'south', 'east', 'west']
 chosen_exit = ''
 while chosen_exit not in available_exits:
     chosen_exit = input('Please choose a direction: ')
+    if chosen_exit == 'quit':
+        print('Game over')
+        break
 
 print('Aren\'t you glad to got out of there?')
