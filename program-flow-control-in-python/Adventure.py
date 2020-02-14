4154f8fb55017621792f94acb16d6e5e178199e5
michalkasiarz <michal.kasiarz@post.com>
2020-02-14 10:40:07 +0100

    adventure mini-game little refactor

--- program-flow-control-in-python/Adventure.py
+++ program-flow-control-in-python/Adventure.py
@@ -3,7 +3,7 @@ available_exits = ['north', 'south', 'east', 'west']
 chosen_exit = ''
 while chosen_exit not in available_exits:
     chosen_exit = input('Please choose a direction: ')
-    if chosen_exit == 'quit':
+    if chosen_exit.casefold() == 'quit':
         print('Game over')
         break
 
