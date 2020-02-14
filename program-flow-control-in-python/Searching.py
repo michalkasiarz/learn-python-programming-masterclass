8769c2da4c4cc6e5430e1641692348fb345e6c09
michalkasiarz <michal.kasiarz@post.com>
2020-02-14 08:53:35 +0100

    break statement added to the loop

--- program-flow-control-in-python/Searching.py
+++ program-flow-control-in-python/Searching.py
@@ -9,5 +9,6 @@ found_at = None
 for index in range(len(shopping_list)):
     if shopping_list[index] == item_to_find:
         found_at = index
+        break   # if an item is found, the loop is ended
 
 print('Item found at position {}.'.format(found_at))
