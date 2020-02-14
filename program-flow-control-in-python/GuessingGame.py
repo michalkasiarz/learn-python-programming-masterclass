8ced823471613409bbd14e9b43b035b914a82d52
michalkasiarz <michal.kasiarz@post.com>
2020-02-14 13:23:08 +0100

    Guessing Game refactor - keep guessing option added, challenge decription as well

--- program-flow-control-in-python/GuessingGame.py
+++ program-flow-control-in-python/GuessingGame.py
@@ -1,3 +1,10 @@
+# Challenge
+# Modify the program to use a while loop, to allow the player to keep guessing.
+# The program should let the player know whether to guess higher or lower, and should
+# print a message when the guess is correct.
+# A correct guess will terminate the program
+# As an optional extra, allow the player to quit by entering 0 (zero) for the guess
+
 import random
 
 # assigns a random number from 1 to 10 to answer variable
@@ -7,20 +14,21 @@ print(answer)   # TODO: remove after answering
 
 print('Please guess a number between 1 and {}: '.format(highest))
 guess = int(input())
+notCorrectGuess = True
 
-if guess < answer:
-    print('Too low!')
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it!")
+while notCorrectGuess:
+    if guess < answer:
+        print('Too low!')
+        guess = int(input())
+        if guess == answer:
+            print("Well done, you guessed it!")
+            break
+    elif guess > answer:
+        print('Too high!')
+        guess = int(input())
+        if guess == answer:
+            print("Well done, you guessed it!")
+            break
     else:
-        print('Sorry, you\'ve not guessed correctly')
-elif guess > answer:
-    print('Too high!')
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it!")
-    else:
-        print('Sorry, you\'ve not guessed correctly')
-else:
-    print('You got it first time!')
+        print('You got it first time!')
+        break
