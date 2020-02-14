7b909afcacecda9ebaca7967a857ec711d62b074
michalkasiarz <michal.kasiarz@post.com>
2020-02-14 10:58:09 +0100

    break challenge

---
+++ program-flow-control-in-python/BreakChallenge.py
@@ -0,0 +1,8 @@
+# Break challenge
+
+# Modify the code inside this loop to stop when it is exactly divisible by 11
+for i in range(0, 100, 7):
+    print(i)
+    if i % 11 == 0 and i != 0:
+        break
+
