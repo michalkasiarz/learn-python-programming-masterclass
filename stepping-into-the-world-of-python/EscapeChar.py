2a2dad12b6a0df07216bb2e1b8e6cd51cbea628f
michalkasiarz <michal.kasiarz@post.com>
2020-02-15 09:12:31 +0100

    refactoring code - variable names changed in accordance with PEP8

--- stepping-into-the-world-of-python/EscapeChar.py
+++ stepping-into-the-world-of-python/EscapeChar.py
@@ -1,18 +1,18 @@
-splitString = "This string has been\nsplit over\nseveral\nlines"
-print(splitString)
+split_string = "This string has been\nsplit over\nseveral\nlines"
+print(split_string)
 
-tabbedString = "1\t2\t3\t4\t5"
-print(tabbedString)
+tabbed_string = "1\t2\t3\t4\t5"
+print(tabbed_string)
 
 print('The pet owner said "No, no, \'e\'s uh,... he\'s resting.')
 print("""The pet owner said "No, no, 'e's uh,... he's resting.""")
 
-anotherSplitString = """This string has been
+another_split_string = """This string has been
 split over
 several
 lines"""
 
-print(anotherSplitString)
+print(another_split_string)
 
 print('Number 1\tThe Larch\nNumber 2\tThe Horse Chestnut')
 print('C:\\Users\\Tim\\Notes.txt')
