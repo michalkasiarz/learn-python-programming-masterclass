d0373faf8602db490a7fcbe3f09e301da61ffa0c
michalkasiarz <michal.kasiarz@post.com>
2020-02-15 10:45:52 +0100

    Tim's own refactor of summary challenge solution

--- program-flow-control-in-python/TimSummaryChallenge.py
+++ program-flow-control-in-python/TimSummaryChallenge.py
@@ -1,16 +1,18 @@
 # Tim's summary challenge solution
 
-print('Please choose: '
-      '\n1 - Learn Python'
-      '\n2 - Learn Java'
-      '\n3 - Go to sleep'
-      '\n4 - Go home!'
-      '\n5 - Exit')
+choice = '-'
 
 while True:
-    choice = input()
-
     if choice == '0':
         break
     elif choice in '12345':
         print('You chose {}'.format(choice))
+    else:
+        print('Please choose: '
+              '\n1 - Learn Python'
+              '\n2 - Learn Java'
+              '\n3 - Go to sleep'
+              '\n4 - Go home!'
+              '\n0 - Exit')
+
+    choice = input()
