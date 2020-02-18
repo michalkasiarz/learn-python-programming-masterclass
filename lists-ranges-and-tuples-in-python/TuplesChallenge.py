479247c0d533d7fc6b03cb408b2d328b6b4ae20f
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-18 18:29:49 +0100

    my solution to tuples challenge

--- lists-ranges-and-tuples-in-python/TuplesChallenge.py
+++ lists-ranges-and-tuples-in-python/TuplesChallenge.py
@@ -1,6 +1,6 @@
 # Tuples Challenge
 
-# Given the tuple below that represents the Imelda May track
+# Given the tuple below that represents the Imelda May album
 # "More Mayhem", write code to print the album details, followed by
 # a listing of all the track in the album.
 
@@ -12,3 +12,10 @@
 imelda = "More Mayhem", "Imelda May", 2011, (
     (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz")
 )
+
+name, artist, year, tracks = imelda
+print(f"Album details:\n"
+      f"Title: {name},\n"
+      f"Artist: {artist},\n"
+      f"Year: {year},\n"
+      f"Tracks: \n\t{tracks[0][1]},\n\t{tracks[1][1]},\n\t{tracks[2][1]},\n\t{tracks[3][1]}.")
