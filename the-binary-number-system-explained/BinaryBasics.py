45072fb1c420feb1e28db3637a63a9310bb04c3a
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-18 19:04:59 +0100

    decimal to binary up to 16

--- the-binary-number-system-explained/BinaryBasics.py
+++ the-binary-number-system-explained/BinaryBasics.py
@@ -1,2 +1,7 @@
 # Binary basics - introduction to the section
 
+# binary is based on two: 0 and 1
+
+# decimal to binary up to 16
+for i in range(17):
+    print("{0:>2} in binary is {0:>08b}".format(i))
