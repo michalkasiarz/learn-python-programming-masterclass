98f50881ebb71c845c4f6cd3bfd65023a840f1e8
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-18 18:08:17 +0100

    Tuples challenge

---
+++ lists-ranges-and-tuples-in-python/TuplesChallenge.py
@@ -0,0 +1,14 @@
+# Tuples Challenge
+
+# Given the tuple below that represents the Imelda May track
+# "More Mayhem", write code to print the album details, followed by
+# a listing of all the track in the album.
+
+#
+# Indent the tracks by a single tab when printing them (remember
+# that you can pass more than one item to print the function,
+# separating them with a comma).
+
+imelda = "More Mayhem", "Imelda May", 2011, (
+    (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz")
+)
