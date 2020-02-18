f2f80a177d0730ad1c47b880f6d097b122dda8e4
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-18 17:49:39 +0100

    tuples unpacking - tracklist

---
+++ lists-ranges-and-tuples-in-python/TuplesTracklist.py
@@ -0,0 +1,15 @@
+# Tuples - tracklist
+
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011, (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Something")
+
+title, artist, year, track1, track2, track3, track4 = imelda
+print(title)
+print(artist)
+print(year)
+print(track1)
+print(track2)
+print(track3)
+print(track4)
