b434ad1121a422b52965aa3789f55b447ed96cfe
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-20 18:04:13 +0100

    typo corrected

--- python-dictionaries-and-sets/Dictionaries.py
+++ python-dictionaries-and-sets/Dictionaries.py
@@ -1,6 +1,6 @@
 # Introduction to dictionaries
 
-fruit = {"oragne": "a sweet, orange, citrus fruit",
+fruit = {"orange": "a sweet, orange, citrus fruit",
          "apple": "good for making cider",
          "lemon": "a sour, yellow citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
