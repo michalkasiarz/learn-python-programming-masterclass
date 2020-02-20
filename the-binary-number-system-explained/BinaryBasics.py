14ec3cd1fb30f027bdd0f73c62612f4b6168049d
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-20 17:16:10 +0100

    decimal to hex

--- the-binary-number-system-explained/BinaryBasics.py
+++ the-binary-number-system-explained/BinaryBasics.py
@@ -5,3 +5,14 @@
 # decimal to binary up to 16
 for i in range(17):
     print("{0:>2} in binary is {0:>08b}".format(i))
+
+# decimal to hex
+for i in range(17):
+    print("{0:>2} in hex is {0:>02x}.".format(i))
+
+x = 0x20
+y = 0x0a
+
+print(x)
+print(y)
+print(x * y)
