24f0d903e8cd0bd6fd54df9577c9f27b5518a90c
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-20 17:38:39 +0100

    accessing elements in dictionary

--- python-dictionaries-and-sets/Dictionaries.py
+++ python-dictionaries-and-sets/Dictionaries.py
@@ -1,1 +1,12 @@
 # Introduction to dictionaries
+
+fruit = {"oragne": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour, yellow citrus fruit",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit"}
+
+print(fruit)
+
+# accessing an element in a dict with a key
+print(fruit["lemon"])
