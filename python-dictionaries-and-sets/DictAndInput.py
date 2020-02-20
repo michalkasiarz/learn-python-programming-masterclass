667309796211fda7b435f43486ba909e2f7eba7b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-20 18:09:40 +0100

    validating input for non-existing objects

--- python-dictionaries-and-sets/DictAndInput.py
+++ python-dictionaries-and-sets/DictAndInput.py
@@ -12,7 +12,10 @@ while True:
     dict_key = input("Please enter a fruit: ")
     if dict_key == "quit":
         break
-    description = fruit.get(dict_key)
-    print(description)
+    if dict_key in fruit:
+        description = fruit.get(dict_key)
+        print(description)
+    else:
+        print("We don't have a " + dict_key)
 
 
