330be58034788e67871769eccd82b5da7374e9cd
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-26 17:33:50 +0100

    Dictionary and join - game

---
+++ python-dictionaries-and-sets/DictAndJoin.py
@@ -0,0 +1,23 @@
+# Dictionary and join
+
+# dictionary of locations
+
+locations = {
+    0: "You are sitting in front of a computer learning Python",
+    1: "You are standing at the end of a road before a small brick building",
+    2: "You are at the top of a hill",
+    3: "You are inside a building, a well house for a small stream",
+    4: "You are in a valley beside a stream",
+    5: "You are in the forest"
+}
+
+"""
+       >     5 Forest
+                ^
+down           down
+2 Hill   <   1 Road   <>   3 Building
+                ^
+                down        
+   ^     <    4 Valley
+"""
+
