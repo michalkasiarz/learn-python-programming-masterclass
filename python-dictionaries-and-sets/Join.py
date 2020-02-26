363d8517aad40891ebe0d75e416a9be229e1ab61
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-26 17:23:46 +0100

    join and dict

--- python-dictionaries-and-sets/Join.py
+++ python-dictionaries-and-sets/Join.py
@@ -1,7 +1,13 @@
 # Join
 
 myList = ["a", "b", "c", "d"]
+letters = "abcdefghijklmnopqrstuvwxyz"
+numbers = "123456789"
 
 newString = ", ".join(myList)
 
 print(newString)
+
+numbersWithJoin = " mississippi ".join(numbers)
+
+print(numbersWithJoin)
