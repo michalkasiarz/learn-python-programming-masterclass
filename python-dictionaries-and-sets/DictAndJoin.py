5c04986d79ae6c4d4c6588b1f710197c47fa0211
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-26 17:36:02 +0100

    game finished

--- python-dictionaries-and-sets/DictAndJoin.py
+++ python-dictionaries-and-sets/DictAndJoin.py
@@ -21,3 +21,25 @@ down           down
    ^     <    4 Valley
 """
 
+exits = [{"Q": 0},
+         {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         {"N": 5, "Q": 0},
+         {"W": 1, "Q": 0},
+         {"N": 1, "W": 2, "Q": 0},
+         {"W": 2, "S": 1, "Q": 0}]
+
+loc = 1
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+
+    direction = input("Available exits are " + availableExits + " ").upper()
+    print()
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannot go in that direction")
