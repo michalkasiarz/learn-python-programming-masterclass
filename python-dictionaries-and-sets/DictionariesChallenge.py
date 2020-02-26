8cbcb493b8bb1626849ff26543ccb893bdc5324b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-02-26 17:44:31 +0100

    dictionaries challenge

---
+++ python-dictionaries-and-sets/DictionariesChallenge.py
@@ -0,0 +1,50 @@
+# Modify the program so that the exits is a dictionary rather than a list,
+# with the keys being the numbers of the locations and the values being
+# dictionaries holding the exits (as they do at present). No change should
+# be needed to the actual code.
+#
+# Once that is working, create another dictionary that contains words that
+# players may use. These words will be the keys, and their values will be
+# a single letter that the program can use to determine which way to go.
+
+locations = {
+    0: "You are sitting in front of a computer learning Python",
+    1: "You are standing at the end of a road before a small brick building",
+    2: "You are at the top of a hill",
+    3: "You are inside a building, a well house for a small stream",
+    4: "You are in a valley beside a stream",
+    5: "You are in the forest"
+}
+
+"""
+       >     5 Forest
+                ^
+down           down
+2 Hill   <   1 Road   <>   3 Building
+                ^
+                down        
+   ^     <    4 Valley
+"""
+
+exits = [{"Q": 0},
+         {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         {"N": 5, "Q": 0},
+         {"W": 1, "Q": 0},
+         {"N": 1, "W": 2, "Q": 0},
+         {"W": 2, "S": 1, "Q": 0}]
+
+loc = 1
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+
+    direction = input("Available exits are " + availableExits + " ").upper()
+    print()
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannot go in that direction")
