ab1f57549da131bf052d5bacd6064861a071e670
michalkasiarz <michal.kasiarz@post.com>
2020-03-01 10:23:11 +0100

    Tim's solution to dictionaries challenge

---
+++ python-dictionaries-and-sets/DictionariesChallengeTim.py
@@ -0,0 +1,49 @@
+# Tim's solution
+
+locations = {0: "You are sitting in front of a computer learning Python",
+             1: "You are standing at the end of a road before a small brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside a building, a well house for a small stream",
+             4: "You are in a valley beside a stream",
+             5: "You are in the forest"}
+
+exits = {0: {"Q": 0},
+         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         2: {"N": 5, "Q": 0},
+         3: {"W": 1, "Q": 0},
+         4: {"N": 1, "W": 2, "Q": 0},
+         5: {"W": 2, "S": 1, "Q": 0} }
+
+vocabulary = { "QUIT":  "Q",
+               "NORTH": "N",
+               "SOUTH": "S",
+               "EAST":  "E",
+               "WEST":  "W"}
+
+# print(locations[0].split())
+# print(locations[3].split(","))
+# print(' '.join(locations[0].split()))
+
+loc = 1
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+
+    direction = input("Available exits are " + availableExits + " ").upper()
+    print()
+    # Parse the user input, using our vocabulary dictionary if necessary
+    if len(direction) > 1:   # more than one letter, so check vocab
+        words = direction.split()
+        for word in words:
+            if word in vocabulary:
+                direction = vocabulary[word]
+                break
+
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannot go in that direction")
