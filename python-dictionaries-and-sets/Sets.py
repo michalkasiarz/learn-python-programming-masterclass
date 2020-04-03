464b0ac8c621c61fca19a0c615884be079101dc8
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-04-03 16:04:53 +0200

    introduction to sets

--- python-dictionaries-and-sets/Sets.py
+++ python-dictionaries-and-sets/Sets.py
@@ -1,1 +1,20 @@
-# Introduction to sets
+# Sets
+
+farm_animals = {"sheep", "cow", "hen"}
+print(farm_animals)
+
+for animal in farm_animals:
+    print(animal)
+
+print("=" * 40)
+
+wild_animals = set(["lion", "tiger", "panther", "elephant", "hare"])
+print(wild_animals)
+
+for animal in wild_animals:
+    print(animal)
+
+farm_animals.add("horse")
+wild_animals.add("horse")
+print(farm_animals)
+print(wild_animals)
