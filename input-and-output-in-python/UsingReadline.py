0c4e8adcf1d32bda24f7f9dd37dc6277a3047914
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-11 11:54:11 +0200

    using readline()

---
+++ input-and-output-in-python/UsingReadline.py
@@ -0,0 +1,5 @@
+with open(r"C:\Users\micha\Documents\buchalka-python\learn-python-programming-masterclass\input-and-output-in-python\original.txt", "r") as jabber:
+    line = jabber.readline()
+    while line:
+        print(line, end="")
+    #    line = jabber.readline()
