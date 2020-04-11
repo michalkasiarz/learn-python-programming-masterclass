ab2ed6133252c48eae3ea89ca83fd7ac358a3aba
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-11 14:38:47 +0200

    stripping strings

---
+++ input-and-output-in-python/ScratchPad.py
@@ -0,0 +1,6 @@
+# Scratch Pad
+
+# strip removes chars from the beginning or end of the string
+print("Adelaide".strip("e"))    # Adelaid
+print("Adelaide".strip("A"))    # delaide
+print("Adelaide".strip("la"))   # Adelaide
