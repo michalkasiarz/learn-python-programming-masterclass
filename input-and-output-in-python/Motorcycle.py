8a73d59d9f96518730d7d17a43df59d90426cf0f
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-14 08:54:01 +0200

    shelve files

---
+++ input-and-output-in-python/Motorcycle.py
@@ -0,0 +1,12 @@
+import shelve
+
+# Motorcycle example
+
+with shelve.open("bike") as bike:
+    bike["make"] = "Honda"
+    bike["model"] = "250 dream"
+    bike["color"] = "red"
+    bike["engine_size"] = 250
+
+    print(bike["engine_size"])
+    print(bike["color"])
