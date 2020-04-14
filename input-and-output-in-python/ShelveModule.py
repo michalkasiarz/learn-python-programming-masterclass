000b212ff5a3b653267e80d1ad139a9b5fae3af3
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-14 09:13:21 +0200

    minor changes

--- input-and-output-in-python/ShelveModule.py
+++ input-and-output-in-python/ShelveModule.py
@@ -2,14 +2,21 @@ import shelve
 
 # Shelve module
 
-with shelve.open("ShelfTest") as fruit:
-    fruit["orange"] = "a sweet, orange, citrus fruit"
-    fruit["apple"] = "good for making cider"
-    fruit["lemon"] = "a sour, yellow, citrus fruit"
-    fruit["grape"] = "a small, sweet fruit growing in bunches"
-    fruit["lime"] = "a sour, green citrus fruit"
+fruit = shelve.open("ShelfTest")
+fruit["orange"] = "a sweet, orange, citrus fruit"
+fruit["apple"] = "good for making cider"
+fruit["lemon"] = "a sour, yellow, citrus fruit"
+fruit["grape"] = "a small, sweet fruit growing in bunches"
+fruit["lime"] = "a sour, green citrus fruit"
 
-    print(fruit["lemon"])
-    print(fruit["grape"])
+print(fruit["lemon"])
+print(fruit["grape"])
+
+fruit["lime"] = "great with tequila"
+
+for snack in fruit:
+    print(snack + ": " + fruit[snack])
+
+fruit.close()
 
 print(fruit)
