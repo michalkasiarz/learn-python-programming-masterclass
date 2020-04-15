282a84f883b9c35e027def72b21fe3b739e1dfe4
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-15 08:37:38 +0200

    updating with shelve

--- input-and-output-in-python/Recipes.py
+++ input-and-output-in-python/Recipes.py
@@ -2,16 +2,32 @@
 
 import shelve
 
-blt = ["bacon", "lettuce", "tomato", "bread"]
-beans_on_toast = ["beans", "bread"]
-scrambled_eggs = ["eggs", "butter", "milk"]
+# blt = ["bacon", "lettuce", "tomato", "bread"]
+# beans_on_toast = ["beans", "bread"]
+# scrambled_eggs = ["eggs", "butter", "milk"]
 soup = ["tip of soup"]
-pasta = ["pasta", "cheese"]
+# pasta = ["pasta", "cheese"]
 
-with shelve.open("recipes") as recipes:
-    recipes["blt"] = blt
-    recipes["beans on toast"] = beans_on_toast
-    recipes["scrambled egg"] = scrambled_eggs
-    recipes["pasta"] = pasta
+with shelve.open("recipes", writeback=True) as recipes:
+    # recipes["blt"] = blt
+    # recipes["beans on toast"] = beans_on_toast
+    # recipes["scrambled egg"] = scrambled_eggs
+    # recipes["pasta"] = pasta
+    # recipes["soup"] = soup
+
+    # recipes["blt"].append("butter")
+    # recipes["pasta"].append("tomato")
+    # temp_list = recipes["blt"]
+    # temp_list.append("butter")
+    # recipes["blt"] = temp_list
+    #
+    # temp_list = recipes["pasta"]
+    # temp_list.append("tomato")
+    # recipes["pasta"] = temp_list
+
+    recipes["soup"].append("croutch")
+
+    for snack in recipes:
+        print(snack, recipes[snack])
 
 
