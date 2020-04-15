6fe035c083b625da9bef5b24773039340249fa46
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-15 08:48:53 +0200

    shelve challenge definition added

---
+++ input-and-output-in-python/ShelveChallenge.py
@@ -0,0 +1,12 @@
+# Modify the program from the Second Dictionary challenge of lecture 56
+# to use shelves instead of dictionaries.
+#
+# Do this by creating two programs. cave_initialise.py should create the two
+# shelves (locations and vocabulary) with the appropriate keys and values.
+#
+# cave_game.py will then use the two shelves instead of dictionaries.
+# Apart from opening and closing the shelves, cave_game will need only
+# two changes to the actual code - remember that shelf keys MUST be strings!
+#
+# Just to be clear, cave_game.py will contain the code from line 45, everything
+# before that (modified to use shelves) will be in cave_initialise.py.
