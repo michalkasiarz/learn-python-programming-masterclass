f6385b9612ee70c4657c322bda583c90807751d2
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-16 08:06:41 +0200

    basics of turtle module

---
+++ modules-and-functions-in-python/TurtleDemo.py
@@ -0,0 +1,14 @@
+# Turtle demo
+
+import turtle
+
+
+# IDE bug: ignoring inspection bug for turtle module
+# noinspection PyUnresolvedReferences
+
+# using turtle module
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(150)
+
+turtle.done()
