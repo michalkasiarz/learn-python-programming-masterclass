a399715486354d5f8b9b9a730a498926d3989804
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-16 08:36:21 +0200

    using dir()

---
+++ modules-and-functions-in-python/Example.py
@@ -0,0 +1,13 @@
+import shelve
+
+# printing built-in functions
+
+for f in dir(__builtins__):
+    print(f)
+
+print("=" * 40)
+
+print("SHELVE FUNCTIONS:")
+for obj in dir(shelve.Shelf):
+    if obj[0] != "_":
+        print(obj)
