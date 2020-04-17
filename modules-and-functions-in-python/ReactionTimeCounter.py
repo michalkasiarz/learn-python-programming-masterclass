a28fbfe35e0427165a9774924c1d33243dc1965a
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-17 09:52:05 +0200

    reaction time counter

---
+++ modules-and-functions-in-python/ReactionTimeCounter.py
@@ -0,0 +1,19 @@
+import time
+from time import  time as my_timer
+import random
+
+# Counts the reaction time of the user
+
+input("Press enter to start!")
+
+wait_time = random.randint(1, 6)
+time.sleep(wait_time)
+start_time = my_timer()
+input("Press enter to stop!")
+
+end_time = my_timer()
+
+print("Started at " + time.strftime("%X", time.localtime(start_time)))
+print("Ended at " + time.strftime("%X", time.localtime(end_time)))
+
+print("Your reaction time was {} seconds!".format(end_time - start_time))
