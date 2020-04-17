e34c440ed8f7e3d78c84ff89709f1fd061852b9b
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-17 09:46:22 +0200

    experimenting with time module

--- modules-and-functions-in-python/DateCalc.py
+++ modules-and-functions-in-python/DateCalc.py
@@ -4,6 +4,11 @@ import time
 
 print(time.gmtime(0))
 
-print(time.localtime(time.time()))
+# using localtime method
+time_here = time.localtime()
+print(time)
 
-print(time.time())
+# Two ways of printing year, month and day
+print("Year: ", time_here[0], time_here.tm_year)
+print("Month: ", time_here[1], time_here.tm_mon)
+print("Day: ", time_here[2], time_here.tm_mday)
