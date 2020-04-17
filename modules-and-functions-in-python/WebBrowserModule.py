12c2d116f874aee87c9463f03e540309906a53ac
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-17 09:30:03 +0200

    experimenting with WebBrowser module - TODO: look up for browser.get for other browsers

--- modules-and-functions-in-python/WebBrowserModule.py
+++ modules-and-functions-in-python/WebBrowserModule.py
@@ -5,3 +5,7 @@ import webbrowser
 webbrowser.open("https://www.python.org", new=1, autoraise=True)
 
 help(webbrowser)
+
+chrome = webbrowser.get(using="windows-default")    # TODO: solve the problem of locating the runnable of other than default browser
+chrome.open_new("https://www.google.com")
+
