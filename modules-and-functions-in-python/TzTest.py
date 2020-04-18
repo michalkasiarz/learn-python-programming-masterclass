cb32f4896e084b141c29919f9abd6eb1c71bafa8
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-18 09:33:03 +0200

    printing timezone for a country

--- modules-and-functions-in-python/TzTest.py
+++ modules-and-functions-in-python/TzTest.py
@@ -7,3 +7,12 @@ tz_to_display = pytz.timezone(country)
 local_time = datetime.datetime.now(tz=tz_to_display)
 print("The time in {} is {}".format(country, local_time))
 print("UTC is {}".format(datetime.datetime.utcnow()))
+
+for x in pytz.all_timezones:
+    print(x)
+
+for x in sorted(pytz.country_names):
+    print(x + ": " + pytz.country_names[x])
+
+for x in sorted(pytz.country_names):
+    print("{}: {}: {}".format(x, pytz.country_names[x], pytz.country_timezones.get(x)))
