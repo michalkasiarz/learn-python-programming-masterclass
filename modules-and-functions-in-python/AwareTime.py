acff317e8d8339222a2b2e291b83ab440958a537
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-19 12:23:36 +0200

    aware time

---
+++ modules-and-functions-in-python/AwareTime.py
@@ -0,0 +1,14 @@
+import datetime
+import pytz
+
+local_time = datetime.datetime.now()
+utc_time = datetime.datetime.utcnow()
+
+print("Naive local time {}".format(local_time))
+print("Naive UTC {}".format(utc_time))
+
+aware_local_time = pytz.utc.localize(local_time)
+aware_utc_time = pytz.utc.localize(utc_time)
+
+print("Aware local time {}, time zone {}".format(aware_local_time, aware_local_time.tzinfo))
+print("Aware UTC {}, time zone {}".format(aware_utc_time, aware_utc_time.tzinfo))
