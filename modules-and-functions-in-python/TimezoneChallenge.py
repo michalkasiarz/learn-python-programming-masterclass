d7de44956df029dd7b9d3a3babc9ba9e83a34417
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-19 12:36:35 +0200

    timezone challenge added

---
+++ modules-and-functions-in-python/TimezoneChallenge.py
@@ -0,0 +1,13 @@
+# Create a program that allows a user to choose one of
+# up to 9 time zones from a menu. You can choose any
+# zones you want from the all_timezones list.
+#
+# The program will then display the time in that timezone, as
+# well as local time and UTC time.
+#
+# Entering 0 as the choice will quit the program.
+#
+# Display the dates and times in a format suitable for the
+# user of your program to understand, and include the
+# timezone name when displaying the chosen time.
+
