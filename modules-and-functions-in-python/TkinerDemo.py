b878b0c1ba0a7823933bd6587c104309c441bf03
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-19 13:20:00 +0200

    adding a label and text to the window

--- modules-and-functions-in-python/TkinerDemo.py
+++ modules-and-functions-in-python/TkinerDemo.py
@@ -5,13 +5,15 @@ try:
 except ImportError:     # Python 2
     import Tkinter as tkinter
 
-print(tkinter.TkVersion)
-print(tkinter.TclVersion)
-
-# tkinter._test()
-
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Hello world!")
-mainWindow.geometry("640x480")
+mainWindow.geometry("640x480+800+400")
+
+label = tkinter.Label(mainWindow, text="Hello world!")
+label.pack(side="top")
+
+canvas = tkinter.Canvas(mainWindow, relief="raised", borderwidth=1)
+canvas.pack(side="left")
+
 mainWindow.mainloop()
