a76510ef3faae3f4ab42fd482c113eedfeafd037
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-19 12:28:01 +0200

    using timestamp

--- modules-and-functions-in-python/AwareTime.py
+++ modules-and-functions-in-python/AwareTime.py
@@ -7,8 +7,13 @@ utc_time = datetime.datetime.utcnow()
 print("Naive local time {}".format(local_time))
 print("Naive UTC {}".format(utc_time))
 
-aware_local_time = pytz.utc.localize(local_time)
+aware_local_time = pytz.utc.localize(utc_time).astimezone()
 aware_utc_time = pytz.utc.localize(utc_time)
 
 print("Aware local time {}, time zone {}".format(aware_local_time, aware_local_time.tzinfo))
 print("Aware UTC {}, time zone {}".format(aware_utc_time, aware_utc_time.tzinfo))
+
+gap_time = datetime.datetime(2015, 10, 25, 1, 30, 0, 0)
+print(gap_time)
+print(gap_time.timestamp())
+
