358ccfada0d302e950a091ec7a0344799e0bcdd6
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-19 13:37:33 +0200

    buttons added

--- modules-and-functions-in-python/TkinerDemo.py
+++ modules-and-functions-in-python/TkinerDemo.py
@@ -14,6 +14,14 @@ label = tkinter.Label(mainWindow, text="Hello world!")
 label.pack(side="top")
 
 canvas = tkinter.Canvas(mainWindow, relief="raised", borderwidth=1)
-canvas.pack(side="left")
+canvas.pack(side="top")
+
+button1 = tkinter.Button(mainWindow, text="button1")
+button2 = tkinter.Button(mainWindow, text="button2")
+button3 = tkinter.Button(mainWindow, text="button3")
+
+button1.pack(side="top", anchor="n")
+button2.pack(side="top", anchor="s")
+button3.pack(side="top", anchor="e")
 
 mainWindow.mainloop()
