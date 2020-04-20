6c99641780556b5fb113085cc399ab405015fbee
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-20 07:53:40 +0200

    configuring columns

--- modules-and-functions-in-python/TkinerDemo.py
+++ modules-and-functions-in-python/TkinerDemo.py
@@ -23,7 +23,7 @@ canvas.grid(row=1, column=0)
 
 # right frame
 rightFrame = tkinter.Frame(mainWindow)
-rightFrame.grid(row=1, column=2)
+rightFrame.grid(row=1, column=2, sticky="n")
 
 button1 = tkinter.Button(rightFrame, text="button1")
 button2 = tkinter.Button(rightFrame, text="button2")
@@ -33,4 +33,16 @@ button1.grid(row=0, column=0)
 button2.grid(row=1, column=0)
 button3.grid(row=2, column=0)
 
+# configure the columns
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.grid_columnconfigure(2, weight=1)
+
+leftFrame.config(relief="sunken", borderwidth=1)
+rightFrame.config(relief="sunken", borderwidth=1)
+leftFrame.grid(sticky="ns")
+rightFrame.grid(sticky="new")
+
+rightFrame.columnconfigure(0, weight=1)
+button2.grid(sticky="ew")
 mainWindow.mainloop()
