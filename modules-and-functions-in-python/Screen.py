3a1312034762b2c5010d51a33e7d502ff3e75dbc
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-21 09:57:08 +0200

    column conf added

--- modules-and-functions-in-python/Screen.py
+++ modules-and-functions-in-python/Screen.py
@@ -11,4 +11,12 @@ mainWindow.geometry("640x480-8-200")
 label = tkinter.Label(mainWindow, text="Tkinter Grid Demo")
 label.grid(row=0, column=0, columnspan=3)
 
+# column conf added
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.columnconfigure(2, weight=3)
+mainWindow.columnconfigure(3, weight=3)
+mainWindow.columnconfigure(4, weight=3)
+
+
 mainWindow.mainloop()
