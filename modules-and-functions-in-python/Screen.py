14b464f980cac142b31aed3eee86ac4373817936
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-21 10:47:38 +0200

    listbox added

--- modules-and-functions-in-python/Screen.py
+++ modules-and-functions-in-python/Screen.py
@@ -3,6 +3,8 @@ try:
 except ImportError:  # Python 2
     import Tkinter as tkinter
 
+import os
+
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Grid Demo")
@@ -24,5 +26,13 @@ mainWindow.rowconfigure(1, weight=10)
 mainWindow.rowconfigure(2, weight=1)
 mainWindow.rowconfigure(3, weight=3)
 mainWindow.rowconfigure(4, weight=3)
+
+# listbox added
+fileList = tkinter.Listbox(mainWindow)
+fileList.grid(row=1, column=0, sticky="nsew", rowspan=2)
+fileList.config(border=2, relief="sunken")
+
+for zone in os.listdir("/Windows/System32"):
+    fileList.insert(tkinter.END, zone)
 
 mainWindow.mainloop()
