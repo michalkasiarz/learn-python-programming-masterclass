c273b6a58e162ea8e2f7a2ca4e03ff3d81af185a
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-22 17:57:50 +0200

    label for result added

--- modules-and-functions-in-python/Screen.py
+++ modules-and-functions-in-python/Screen.py
@@ -56,6 +56,12 @@ radio1.grid(row=0, column=0, sticky="w")
 radio2.grid(row=1, column=0, sticky="w")
 radio3.grid(row=2, column=0, sticky="w")
 
+resultLabel = tkinter.Label(mainWindow, text="Result")
+resultLabel.grid(row=2, column=2, sticky="nw")
+result = tkinter.Entry(mainWindow)
+result.grid(row=2, column=2, sticky="sw")
+
+
 mainWindow.mainloop()
 
 # prints the radio button of choice to the console
