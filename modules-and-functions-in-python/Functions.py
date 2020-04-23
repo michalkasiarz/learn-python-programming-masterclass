d469215b62a497f8eb2df1af8aec015804e0a692
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-23 10:33:27 +0200

    introduction to functions

---
+++ modules-and-functions-in-python/Functions.py
@@ -0,0 +1,14 @@
+# Functions
+
+def python_food():
+    width = 80
+    text = "spam and eggs"
+    left_margin = (width - len(text) // 2)
+    print(" " * left_margin, text)
+
+
+# call the function
+python_food()
+
+# returns None
+print(python_food())
