4cd242c8240633be1c76f19b800c4e860887ad89
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-23 10:55:00 +0200

    casting to string added

--- modules-and-functions-in-python/Functions.py
+++ modules-and-functions-in-python/Functions.py
@@ -9,12 +9,15 @@ def python_food():
 
 
 def centre_text(text):
+    # casting to string
+    text = str(text)
     left_margin = (80 - len(text) // 2)
     print(" " * left_margin, text)
 
 
 # call the function
 centre_text("spam and eggs")
+centre_text(12)
 centre_text("spam, spam and eggs")
 centre_text("spam, spam, spam and spam")
 
