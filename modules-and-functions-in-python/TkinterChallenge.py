3bf8d283a95c9c2d36c72ffa4ae406a8877dcb7c
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-23 08:10:21 +0200

    tkinter challenge defined

---
+++ modules-and-functions-in-python/TkinterChallenge.py
@@ -0,0 +1,23 @@
+# Write a GUI program to create a simple calculator
+# layout that looks like the screenshot.
+#
+# Try to be as Pythonic as possible - it's ok if you
+# end up writing repeated Button and Grid statements,
+# but consider using lists and a for loop.
+#
+# There is no need to store the buttons in variables.
+#
+# As an optional extra, refer to the documentation to
+# work out how to use minsize() to prevent your window
+# from being shrunk so that the widgets vanish from view.
+#
+# Hint: You may want to use the widgets .winfo_height() and
+# winfo_width() methods, in which case you should know that
+# they will not return the correct results unless the window
+# has been forced to draw the widgets by calling its .update()
+# method first.
+#
+# If you are using Windows you will probably find that the
+# width is already constrained and can't be resized too small.
+# The height will still need to be constrained, though.
+
