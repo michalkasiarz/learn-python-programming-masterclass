0459e7989f37298d5208073153a19b7d40ab6297
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-23 08:29:46 +0200

    keypad added

--- modules-and-functions-in-python/TkinterChallenge.py
+++ modules-and-functions-in-python/TkinterChallenge.py
@@ -21,3 +21,37 @@
 # width is already constrained and can't be resized too small.
 # The height will still need to be constrained, though.
 
+try:
+    import tkinter
+except ImportError:
+    import Tkinter as tkinter
+
+keys = [[("C", 1, ("CE", 1))],
+        [("7", 1), ("8", 1), ("9", 1), ("+", 1)],
+        [("4", 1), ("5", 1), ("6", 1), ("-", 1)],
+        [("1", 1), ("2", 1), ("3", 1), ("*", 1)],
+        [("0", 1), ("-", 1), ("/", 1)],
+        ]
+
+mainWindowPadding = 0
+
+mainWindow = tkinter.Tk()
+mainWindow.title("Calculator")
+mainWindow.geometry("640x480-8-200")
+mainWindow["padx"] = mainWindowPadding
+
+result = tkinter.Entry(mainWindow)
+result.grid(row=0, column=0, sticky="nsew")
+
+keyPad = tkinter.Frame(mainWindow)
+keyPad.grid(row=1, column=0, sticky="nsew")
+
+row = 0
+for keyRow in keys:
+    col = 0
+    for key in keyRow:
+        tkinter.Button(keyPad, text=key[0]).grid(row=row, column=col, columnspan=key[1], sticky=tkinter.E + tkinter.W)
+        col += key[1]
+    row += 1
+
+mainWindow.mainloop()
