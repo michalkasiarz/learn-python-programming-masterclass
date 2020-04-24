1f56336270041c51ca6221c1bfd6be896240a97b
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-24 12:53:54 +0200

    multiple arguments passing option added

--- modules-and-functions-in-python/Functions.py
+++ modules-and-functions-in-python/Functions.py
@@ -8,9 +8,10 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def centre_text(text):
-    # casting to string
-    text = str(text)
+def centre_text(*args):
+    text = ""
+    for arg in args:
+        text += str(arg) + " "
     left_margin = (80 - len(text) // 2)
     print(" " * left_margin, text)
 
@@ -20,6 +21,7 @@ centre_text("spam and eggs")
 centre_text(12)
 centre_text("spam, spam and eggs")
 centre_text("spam, spam, spam and spam")
+centre_text("spam, spam, spam and spam", 11, 123123, "go", "hello")
 
 # printing text
 a = "Hello"
