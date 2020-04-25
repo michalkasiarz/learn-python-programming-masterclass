21f41533ad31546155810c3da4ee4839b2193acb
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-25 14:53:00 +0200

    another way of saving the method result to a file

---
+++ modules-and-functions-in-python/FunctionsChanged2.py
@@ -0,0 +1,24 @@
+def python_food():
+    width = 80
+    text = "Spam and eggs"
+    left_margin = (width - len(text)) // 2
+    print(" " * left_margin, text)
+
+
+def centre_text(*args, sep=' '):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
+    left_margin = (80 - len(text)) // 2
+    return " " * left_margin + text
+
+
+with open("menu", "w") as menu:
+    s1 = centre_text("spam and eggs")
+    print(s1, file=menu)
+    s2 = centre_text("spam, spam and eggs")
+    print(s2, file=menu)
+    print(centre_text(12), file=menu)
+    print(centre_text("spam, spam, spam and spam"), file=menu)
+    s5 = centre_text("first", "second", 3, 4, "spam", sep=":")
+    print(s5, file=menu)
