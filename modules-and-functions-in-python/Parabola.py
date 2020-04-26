b7fcfaed3ea2fd4b1bde955422e855ecb2d65b32
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-26 11:09:17 +0200

    finished code on parabola

--- modules-and-functions-in-python/Parabola.py
+++ modules-and-functions-in-python/Parabola.py
@@ -1,25 +1,22 @@
-import math
-
 try:
     import tkinter
-except ImportError:  # python 2
+except ImportError:  # Python 2
     import Tkinter as tkinter
 
 
 def parabola(page, size):
-    for x in range(-size, size):
+    for x in range(size):
         y = x * x / size
         plot(page, x, y)
         plot(page, -x, y)
 
+# Modify the circle function so that it allows the colour of the circle to be specified
+# and defaults to red if a colour is not given.  You may want to review the previous lectures
+# about named parameters and default values.
 
-def circle(page, radius, g, h):
-    for x in range(g, g+radius):
-        y = h + (math.sqrt(radius ** 2 - ((x - g) ** 2)))
-        plot(page, x, y)
-        plot(page, x, 2 * h - y)
-        plot(page, 2 * g - x, y)
-        plot(page, 2 * g - x, 2 * h - y)
+
+def circle(page, radius, g, h, colour="red"):
+    page.create_oval(g + radius, h + radius, g - radius, h - radius, outline=colour, width=2)
 
 
 def draw_axes(page):
@@ -27,16 +24,17 @@ def draw_axes(page):
     x_origin = page.winfo_width() / 2
     y_origin = page.winfo_height() / 2
     page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    page.create_line(-x_origin, 0, x_origin, 0, fill="navy")
-    page.create_line(0, y_origin, 0, -y_origin, fill="navy")
+    page.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    page.create_line(0, y_origin, 0, -y_origin, fill="black")
     print(locals())
 
 
 def plot(page, x, y):
-    page.create_line(x, -y, x + 1, -y + 1, fill="darkgreen")
+    page.create_line(x, -y, x + 1, -y + 1, fill="red")
 
 
 mainWindow = tkinter.Tk()
+
 
 mainWindow.title("Parabola")
 mainWindow.geometry("640x480")
@@ -47,5 +45,15 @@ canvas.grid(row=0, column=0)
 draw_axes(canvas)
 
 parabola(canvas, 100)
+parabola(canvas, 200)
+circle(canvas, 100, 100, 100, "green")
+circle(canvas, 100, 100, -100, "yellow")
+circle(canvas, 100, -100, 100, "black")
+circle(canvas, 100, -100, -100, "blue")
+circle(canvas, 10, 30, 30)
+circle(canvas, 10, 30, -30)
+circle(canvas, 10, -30, 30)
+circle(canvas, 10, -30, -30)
+circle(canvas, 30, 0, 0)
 
 mainWindow.mainloop()
