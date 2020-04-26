62b2e8700a7b6f02566f70c04042a1ad94af1a5d
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-26 10:46:44 +0200

    parabola method changed

--- modules-and-functions-in-python/Parabola.py
+++ modules-and-functions-in-python/Parabola.py
@@ -4,9 +4,10 @@ except ImportError:  # python 2
     import Tkinter as tkinter
 
 
-def parabola(x):
-    y = x * x / 100
-    return y
+def parabola(page, size):
+    for x in range(-size, size):
+        y = x * x / size
+        plot(page, x, y)
 
 
 def draw_axes(page):
@@ -32,7 +33,6 @@ canvas = tkinter.Canvas(mainWindow, width=640, height=480)
 canvas.grid(row=0, column=0)
 
 draw_axes(canvas)
-
 
 for x in range(-100, 100):
     y = parabola(x)
