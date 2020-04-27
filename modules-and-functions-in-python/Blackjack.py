a54257442f5eefd8660e946be90065ed177c2f90
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-27 10:46:10 +0200

    load_imaged method defined

--- modules-and-functions-in-python/Blackjack.py
+++ modules-and-functions-in-python/Blackjack.py
@@ -7,8 +7,32 @@ try:
 except ImportError:
     import Tkinter as tkinter
 
-
 mainWindow = tkinter.Tk()
+
+
+def load_images(card_images):
+    suits = ['heart', 'club', 'diamond', 'spade']
+    face_cards = ['jack', 'queen', 'king']
+
+    if tkinter.TkVersion >= 8.6:
+        extension = "png"
+    else:
+        extension = "ppm"
+
+    # for each suit, retrieve the image for the cards
+    for suit in suits:
+        # first the number cards 1 to 10
+        for card in range(1, 11):
+            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((card, image,))
+
+        # next the face cards
+        for card in face_cards:
+            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((10, image,))
+
 
 # Set up the screen and frames for the dealer and player
 
@@ -46,4 +70,3 @@ dealer_button.grid(row=0, column=0)
 
 player_button = tkinter.Button(button_frame, text="Player")
 player_button.grid(row=0, column=1)
-
