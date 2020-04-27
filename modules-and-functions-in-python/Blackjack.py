7826970141f34b0756bd5867bb2976ca74dc58b1
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-27 11:12:56 +0200

    dealer score label bug fixed

--- modules-and-functions-in-python/Blackjack.py
+++ modules-and-functions-in-python/Blackjack.py
@@ -65,7 +65,7 @@ card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
-tkinter.Label(card_frame, text=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
+tkinter.Label(card_frame, textvariable=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
 
 # embedded frame to hold the card images
 dealer_card_frame = tkinter.Frame(card_frame, background="green")
