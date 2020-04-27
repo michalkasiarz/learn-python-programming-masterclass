7a09b050a8e0c92665066eec7c410d5f4afee657
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-04-27 10:22:15 +0200

    frames added

--- modules-and-functions-in-python/Blackjack.py
+++ modules-and-functions-in-python/Blackjack.py
@@ -14,3 +14,22 @@ mainWindow = tkinter.Tk()
 
 mainWindow.title("Black Jack")
 mainWindow.geometry("640x480")
+
+result_text = tkinter.StringVar()
+result = tkinter.Label(mainWindow, textvariable=result_text)
+result.grid(row=0, column=0, columnspan=3)
+
+card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="green")
+card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
+
+dealer_score_label = tkinter.IntVar()
+tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
+tkinter.Label(card_frame, text=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
+
+# embedded frame hold the card images
+dealer_card_frame = tkinter.Frame(card_frame, background="green")
+dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
+
+player_score_label = tkinter.IntVar()
+tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
+tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
